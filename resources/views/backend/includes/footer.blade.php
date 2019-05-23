26849643ab931b322dbabfe6f853d99ea7db869a
ramesh.srinivasan <rsrinivasan@appointmentplus.com>
2019-05-23 15:34:16 +0530

    first commit

---
+++ resources/views/backend/includes/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="app-footer">
+    <div>
+        <strong>@lang('labels.general.copyright') &copy; {{ date('Y') }}
+            <a href="http://rameshsrinivasan.in" target="_blank">
+                @lang('strings.backend.general.boilerplate_link')
+            </a>
+        </strong> @lang('strings.backend.general.all_rights_reserved')
+    </div>
+</footer>
